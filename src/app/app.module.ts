f5f40d102c8baa3c649fe7cdc2eebd9335553d11
GChiappa <chiappa.giovanni.gc@gmail.com>
2022-07-22 15:07:26 +0200

    Configurato routing -GC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaArticoliComponent } from './components/lista-articoli/lista-articoli.component';
 import { LoginComponent } from './components/login/login.component';
@@ -13,6 +12,26 @@ import { FormArticoloComponent } from './components/admin-components/form-artico
 import { GestioneArticoliComponent } from './components/admin-components/gestione-articoli/gestione-articoli.component';
 import { CarrelloComponent } from './components/carrello/carrello.component';
 import { OrdineComponent } from './components/ordine/ordine.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'carrello', component: CarrelloComponent },
+  { path: 'articoli', component: ListaArticoliComponent },
+  { path: 'listaarticoli', redirectTo: 'articoli' },
+  { path: 'login', component: LoginComponent },
+  { path: 'accedi', redirectTo: 'login' },
+  { path: 'accesso', redirectTo: 'login' },
+  { path: 'signin', component: SigninComponent },
+  { path: 'registrati', redirectTo: 'signin' },
+  { path: 'registrazione', redirectTo: 'signin' },
+  { path: 'ordine', component: OrdineComponent },
+  { path: 'formarticolo', component: FormArticoloComponent },
+  { path: 'gestionearticoli', component: GestioneArticoliComponent }
+];
 
 @NgModule({
   declarations: [
@@ -30,9 +49,9 @@ import { OrdineComponent } from './components/ordine/ordine.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes, { useHash: true })
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
